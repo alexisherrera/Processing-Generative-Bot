fbb85c963352676a9492b1cd4a9eef9cd1753184
alexisherrera <halexis@sas.upenn.edu>
2017-08-24 22:12:21 -0700

    config file

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'xj3u0BfKPzEV3SA6at3Hh8M1i',
+  consumer_secret: 'P80n1ic7Bd95qJYurBvI1GBspFXOubPKTeLIHAZQVzod6aPAsD',
+  access_token: '879375146313814017-9mrO30quAXfeJOm8PdJLHPZ333loF9j',
+  access_token_secret: 'PvhJ3bz2giRHKOjqcjFiUaiToX0asXmNXlH5N2MDTq9uc'
+}
